82de02385a00b6f300e4c5b080c967b0c342c1f0
Leonardo Araujo <leolimalp@hotmail.com>
2022-02-07 16:11:51 -0300

    Class Position created

---
+++ Chess_System/src/Program/App.java
@@ -0,0 +1,10 @@
+package Program;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) {
+        Position pos = new Position(1, 2);
+        System.out.println(pos);
+    }
+}
