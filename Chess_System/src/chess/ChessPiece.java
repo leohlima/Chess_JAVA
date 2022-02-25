8b2e4373a39a55d7ffdb1e8b0ac9f33277d7ff63
Leonardo Araujo <leolimalp@hotmail.com>
2022-02-24 21:09:06 -0300

    Rook possibles moves

--- Chess_System/src/chess/ChessPiece.java
+++ Chess_System/src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
@@ -18,5 +19,10 @@ public abstract class ChessPiece extends Piece {
     @Override
     public boolean[][] possibleMoves() {
         return new boolean[0][];
+    }
+
+    protected boolean isThereOpponentPiece(Position position){
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 }
