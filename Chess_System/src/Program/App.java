a88e9982e4061d86a11d0960ec6587bb0fdd65b1
Leonardo Araujo <leolimalp@hotmail.com>
2022-02-25 10:03:42 -0300

    switching turns

--- Chess_System/src/Program/App.java
+++ Chess_System/src/Program/App.java
@@ -20,7 +20,7 @@ public class App {
         while (true) {
             UI.clearScreen();
             try {
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
--- Chess_System/src/Program/UI.java
+++ Chess_System/src/Program/UI.java
@@ -1,5 +1,6 @@
 package Program;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -49,13 +50,20 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
         for (int i = 0; i < pieces.length; i++){
-            System.out.print((8 - i) + " ");
+            System.out.print((8 - i) + "");
             for (int j = 0; j < pieces.length; j++){
                 printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
